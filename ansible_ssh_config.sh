c3851d4fce5ad10ecb9157690ba787c8e886efeb
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-19 16:51:11 +0100

    Update ansible_ssh_config.sh

--- ansible_ssh_config.sh
+++ ansible_ssh_config.sh
@@ -16,9 +16,10 @@ read -a strarr <<< "$RGs"
 for rg in ${RGs[0]} ; do
   echo $rg
   if ! [ -z $(az vm list -g $rg --query "[].id" -o tsv) ] ; then
-    IPs=$(az vm show -d --ids $(az vm list -g $rg --query "[].id" -o tsv) --query privateIps)
-    for ip in $IPs ; do
-      host=$(echo $ip | tr -d '"')
+    VMs=$(az vm show -d --ids $(az vm list -g $rg --query "[].id" -o tsv) --query [name,privateIps])
+    for vm in $VMs ; do
+      name=$(echo ${vm[0]} | tr -d '"')
+      host=$(echo ${vm[1]} | tr -d '"')
       key=$(echo $host | tr '_' '-')
       # Key vault name is dependant of environment 
       ssh-copy-id -i ~/.ssh/sshkey  -o "StrictHostKeyChecking no" gradesadmin@$host <<< $(az keyvault secret show --name $key --vault-name kv-common-fc-test-001 --query "value")
