69b9acff2eb02045cf474b1c3292288a63bd3456
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-19 14:21:18 +0100

    Update ansible_ssh_config.sh

--- ansible_ssh_config.sh
+++ ansible_ssh_config.sh
@@ -2,17 +2,18 @@
 
 ssh-keygen -b 2048 -t rsa -f /sshkey -q -N ""
 
-export RGs=$(az group list --query [].name -o tsv)
+RGs=$(az group list --query [].name -o tsv)
 IFS=$'\n'
 read -a strarr <<< "$RGs"
-
 for rg in ${RGs[0]} ; do
   echo $rg
-  az vm show -d --ids $(az vm list -g $rg --query "[].id" -o tsv) --query privateIps
+  if ! [ -z $(az vm list -g $rg --query "[].id" -o tsv) ] ; then
+    IPs=$(az vm show -d --ids $(az vm list -g $rg --query "[].id" -o tsv) --query privateIps)
+    for ip in $IPs ; do
+      host=$(echo $ip | tr -d '"')
+      ssh-copy-id -i ~/.ssh/sshkey user@$host
+      ssh -i ~/.ssh/sshkey user@$host
+    done
+  fi
 done
 
-az vm show -d --ids $(az vm list -g $RG --query "[].id" -o tsv) --query privateIps
-
-ssh-copy-id -i ~/.ssh/sshkey user@host
-
-ssh -i ~/.ssh/sshkey user@host
