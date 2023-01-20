3fbc2d643d139f201e6fc6142cca418314437924
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-20 11:29:37 +0100

    Update ansible_ssh_config.sh

--- ansible_ssh_config.sh
+++ ansible_ssh_config.sh
@@ -15,11 +15,11 @@ IFS=$'\n'
 read -a strarr <<< "$VMs"
 for id in $VMs ; do 
   name=$(az vm list --show-details --output tsv --query "[?id == '${id}'].name")
-  key=$(echo $name | tr '_' '-')
+  key=$(echo $name | sed "s/vm/key/g"  | tr '_' '-')
   host=$(az vm list --show-details --output tsv --query "[?id == '${id}'].privateIps")
-  echo name ; echo $key ; echo $host
-  az keyvault secret show --name $key --vault-name kv-common-fc-test-001 --query "value"
+  echo $name ; echo $key ; echo $host
+  #az keyvault secret show --name $key --vault-name kv-common-fc-test-001 --query "value"
   # Key vault name is dependant of environment 
-  ssh-copy-id -i ~/.ssh/sshkey  -o "StrictHostKeyChecking no" gradesadmin@$host <<< $(az keyvault secret show --name $key --vault-name kv-common-fc-test-001 --query "value")
-  ssh -i ~/.ssh/sshkey gradesadmin@$host
+  #ssh-copy-id -i ~/.ssh/sshkey  -o "StrictHostKeyChecking no" gradesadmin@$host <<< $(az keyvault secret show --name $key --vault-name kv-common-fc-test-001 --query "value")
+  #ssh -i ~/.ssh/sshkey gradesadmin@$host
 done
