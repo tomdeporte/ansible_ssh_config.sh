e4541c9f13fa59eca865337346eb74d5c49570b9
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-23 11:08:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 FOLDER=/root/ansible
-if [ -d "$FOLDER" ]; then
-    echo "Ansible is already installed"
-else 
-    sudo mkdir /root/ansible
+#if [ -d "$FOLDER" ]; then
+#    echo "Ansible is already installed"
+#else 
+    mkdir /root/ansible
     
     cd /home/gradesadmin
 
     apt-get install ansible -y
 
     # Install Python 3 and pip.
-    sudo apt-get install -y python3-pip
+    apt-get install -y python3-pip
 
     # Upgrade pip3.
-    sudo pip3 install --upgrade pip
+    pip3 install --upgrade pip
 
     # Install Ansible az collection for interacting with Azure.
     ansible-galaxy collection install azure.azcollection
 
     # Install Ansible modules for Azure
-    sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+     ip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
 
     echo -e "[defaults]\ninventory=/root/ansible/inventory\nremote_user=root\nhost_key_checking=False\nbecome=True\nbecome_user=root\nbecome_ask_pass=False\n" >> /root/ansible/ansible.cfg
 
@@ -31,6 +31,6 @@ else
     echo "export ANSIBLE_CONFIG=/root/ansible/ansible.cfg" >> ~/.profile
 
     source ~/.profile
-fi
+#fi
 
 
